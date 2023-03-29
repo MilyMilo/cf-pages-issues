61cbefcfa275d9634db3723eb358ff092a74c2b6
Miłosz Skaza <milosz.skaza@ctfd.io>
2023-03-29 10:25:54 +0200

    test dummy mailjet

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,4 +1,10 @@
 import type { Actions } from './$types';
+import Mailjet from 'node-mailjet';
+
+const mailjet = new Mailjet({
+    apiKey: 'dummy-test',
+    apiSecret: 'dummy-test'
+});
 
 export const actions = {
     default: async ({ fetch, request }) => {
@@ -7,9 +13,16 @@ export const actions = {
         // access data
         console.log(data);
 
-        if (Math.random() >= 0.5)
-            return { success: true };
-
+        if (Math.random() >= 0.5) {
+            await mailjet.post('send').request({
+                FromEmail: 'whatever@example.com',
+                FromName: 'whatever',
+                Subject: 'whatever',
+                'Text-part': 'whatever',
+                Recipients: [{ Email: 'whatever@example.com' }],
+                Headers: { 'Reply-To': 'whatever@example.com' }
+            });
+        }
 
         return { success: false };
     }
